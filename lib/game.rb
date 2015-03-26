d8eceb9518e23bd9050bdc8cc74bc99aec32c28e
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-03-26 14:07:53 +0000

    game can add the first player

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,10 @@
 class Game
 
+  def initialize
+    @number_of_players = 0
+    @players_1_and_2 = []
+  end
+
   def ready?
   end
 
@@ -7,7 +12,16 @@ class Game
   end
 
   def number_of_players
-    0
+    @number_of_players
+  end
+
+  def players_1_and_2
+    @players_1_and_2
+  end
+
+  def add_player(player_name)
+    @number_of_players+=1
+    @players_1_and_2 << player_name
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require 'game'
 
 describe Game do
 
-  it 'When created is not ready'do
+  it 'When created is not ready' do
   expect(subject).not_to be_ready
 end
 
@@ -14,23 +14,59 @@ end
   expect(subject.number_of_players).to eq (0)
 end
 
+  it 'Can add the first player' do
+  game = Game.new
+  game.add_player(:player1)
+  expect(game.number_of_players).to eq 1
+  expect(game.players_1_and_2).to eq [:player1]
+  
+  end
+
+  xit 'Can add the second player' do
+  
+  end
+
+  xit 'Cannot add more than two players' do
+  
+  end
+
+  xit 'Is ready to start when it has two players' do
+  
+  end
+
+    xit 'Knows that player one has the turn' do
+  
+  end
+
+    xit 'Can switch turns' do
+  
+  end
+
+    xit 'Knows that player two has the turn after switching turns' do
+  
+  end
+
+    xit 'Knows the current player' do
+  
+  end
+
+    xit 'Knows the opponent' do
+  
+  end
+
+    xit 'It takes turns to shoot and shoots at the opponents board' do
+  
+  end
+
+    xit 'It knows the winner' do
+  
+  end
+
+    xit 'Cannot switch turns if there is a winner' do
+  
+  end
 end
-
-
-# When created has no players
-#
-# Can add the first player
-# Can add the second player
-# Cannot add more than two players
-# Is ready to start when it has two players
-# Knows that player one has the turn
-# Can switch turns
-# Knows that player two has the turn after switching turns
-# Knows the current player
-# Knows the opponent
-# It takes turns to shoot and shoots at the opponents board
-# It knows the winner
-# Cannot switch turns if there is a winner
+# 
 #
 # Player -  Give him a board
 #           Place a ship
