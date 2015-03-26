a48dbf397e0f2acfafc4dcb6e0dbb0f5524a0d30
paulwallis42 <paulwallis42@gmail.com>
2015-03-26 13:16:53 +0000

    game and game_spec created

---
+++ spec/game_spec.rb
@@ -0,0 +1,30 @@
+require 'game'
+
+describe Game do
+
+  
+
+end
+
+
+
+ When created is not ready
+# When created is not over
+# When created has no players
+#
+# Can add the first player
+# Can add the second player
+# Cannot add more than two players
+# Is ready to start when it has two players
+# Knows that player one has the turn
+# Can switch turns
+# Knows that player two has the turn after switching turns
+# Knows the current player
+# Knows the opponent
+# It takes turns to shoot and shoots at the opponents board
+# It knows the winner
+# Cannot switch turns if there is a winner
+#
+# Player -  Give him a board
+#           Place a ship
+# Add the player to the game
